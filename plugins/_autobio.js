1d170b432d1fa503f7341f469f3f6a162da157d5
mdarbaz786 <152139727+mdarbaz786@users.noreply.github.com>
2024-09-30 23:05:57 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -14,7 +14,7 @@ handler.all = async function (m) {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n⌚ Time Active ${muptime}\n\n ┃ 🛡️ᑭᖇIᑎᑕᕮ ᗷOT ᗰᗪ🛡️`
+		let bio = `\n" ⌚ "+new Date().toLocaleTimeString()+" *🌸⃝⃪꯭⃮⃕⃔⎯꯭᳝֟፝֟Arbaz-Khan🧸͢-≛⃞*
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
   }
